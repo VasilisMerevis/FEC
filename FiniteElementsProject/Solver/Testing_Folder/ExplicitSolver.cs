10ab5106ce88b52699d44a55f075f323615f486a
Vasilis Merevis <vasilis.merevis@outlook.com>
2019-04-17 02:22:48 +0300

    Minor changes and addition of new method to calculate explicit interactions

--- FiniteElementsProject/Solver/Testing_Folder/ExplicitSolver.cs
+++ FiniteElementsProject/Solver/Testing_Folder/ExplicitSolver.cs
@@ -138,6 +138,47 @@ namespace FEC
             return solutionVector;
         }
 
+        private double[] UpdatedF(double[] forceVector)
+        {
+            
+
+            lambda = 1.0 / numberOfLoadSteps;
+            double[] incrementDf = VectorOperations.VectorScalarProductNew(forceVector, lambda);
+            double[] solutionVector = new double[forceVector.Length];
+            double[] incrementalExternalForcesVector = new double[forceVector.Length];
+            double[] tempSolutionVector = new double[solutionVector.Length];
+            double[] deltaU = new double[solutionVector.Length];
+            double[] internalForcesTotalVector;
+            double[] dU;
+
+
+
+            
+            
+                
+                Assembler.UpdateDisplacements(solutionVector);
+
+
+                internalForcesTotalVector = Assembler.CreateTotalInternalForcesVector();
+
+                double[,] tangentMatrix = CalculateHatMMatrix();
+                dU = LinearSolver.Solve(tangentMatrix, incrementDf);
+                solutionVector = VectorOperations.VectorVectorAddition(solutionVector, dU);
+
+                Assembler.UpdateDisplacements(solutionVector);
+                tangentMatrix = CalculateHatMMatrix();
+                internalForcesTotalVector = Assembler.CreateTotalInternalForcesVector();
+
+                residual = VectorOperations.VectorVectorSubtraction(internalForcesTotalVector, incrementalExternalForcesVector);
+                residualNorm = VectorOperations.VectorNorm2(residual);
+                int iteration = 0;
+                Array.Clear(deltaU, 0, deltaU.Length);
+                
+            
+
+            return solutionVector;
+        }
+
         /// <summary>
         /// Calculates accelerations for time t
         /// </summary>
@@ -247,7 +288,7 @@ namespace FEC
                 }
                 else
                 {
-                    nextSolution = LoadControlledNR(hatRVector);
+                    nextSolution = NewtonIterations(hatRVector);
                 }
                 explicitSolution.Add(i, nextSolution);
                 explicitAcceleration.Add(i, CalculateAccelerations());
